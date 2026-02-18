9cd00881b01d6ebca8ba6ed8d1f70fef53004db3
pbhungurde <gundpragati@gmail.com>
2026-02-18 17:10:20 +0530

    Add files via upload

---
+++ Datatypeemploy.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+public class Datatypeemploy {
+	
+	    public static void main(String args[]) {
+ 
+	    	byte age = 35;                 
+	        short employeeId = 1024;      
+	        int experience = 8;           
+	        long mobileNumber = 9876543210L;  
+
+	        float salary = 18.5f;         
+	        double bonus = 2.75;           
+
+	        char grade = 'A';              
+	        boolean isPermanent = true;    
+
+	    
+	        System.out.println("Age : " + age);
+	        System.out.println("Employee ID : " + employeeId);
+	        System.out.println("Experience : " + experience + " years");
+	        System.out.println("Mobile Number : " + mobileNumber);
+	        System.out.println("Salary : " + salary + " LPA");
+	        System.out.println("Bonus : " + bonus + " LPA");
+	        System.out.println("Grade : " + grade);
+	        System.out.println("Permanent Employee : " + isPermanent);
+	    	}
+
+}
