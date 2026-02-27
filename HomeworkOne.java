8678c3fbf742ed9befecdecf69b21e03f331be4c
PragatiGund <gundpragati@gmail.com>
2026-02-27 17:47:28 +0530

    Add files via upload

---
+++ HomeworkOne.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+public class HomeworkOne {
+	
+
+	    public static void main(String[] args) {
+
+	        int a = 40;
+	        System.out.println("" + (a + 20));   // 60
+	        
+
+	        int b = 20;
+	        System.out.println(a + b);    // 60
+
+	        int c = a + b;                // 60
+	        System.out.println(c / a);    // 1
+
+	        int d = c % 2;                // 0
+	        System.out.println(d + 3);    // 3
+
+	        System.out.println(d / c);    // 0
+	    }
+	}
+
+
+
