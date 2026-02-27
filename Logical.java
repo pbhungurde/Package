cc06af341dc8005a1f6d2b0ac9e21095b8de50e9
PragatiGund <gundpragati@gmail.com>
2026-02-27 17:46:47 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+public class Logical {
+  
+	public static void main(String [] args) {
+		
+		System.out.println(" AND operation perform  :");
+		
+		System.out.println(false&&false);
+		
+		System.out.println(true&&true);
+
+		System.out.println(true&&false);
+
+		System.out.println(false&&true);
+		
+		
+		System.out.println("OR operation perform :");
+		
+		System.out.println(false||false);
+		
+		System.out.println(true||true);
+
+		System.out.println(true||false);
+
+		System.out.println(false||true);
+	   
+
+
+          
+		System.out.println("Not equal to :");
+		System.out.println(false != false);
+		
+		System.out.println(true != true);
+
+		System.out.println(true != false);
+
+		System.out.println(false != true);
+	}
+}
