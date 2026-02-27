813e46bad79a6a924b47c9da5c2ec5186629aa8f
PragatiGund <gundpragati@gmail.com>
2026-02-27 17:46:13 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+public class Relational {
+	
+      public static void main(String[]args) {
+    	  
+    	  int a= 4 , b = 10;
+    	  
+    	  boolean c = a != b;
+    	   
+    	  boolean check = true;
+
+    	   
+    	   System.out.println(a>b);
+    	   
+    	   System.out.println(a< 1);
+    	   
+    	   System.out.println(4 >= 4);
+    	   
+    	   System.out.println(4 <= 4);
+    	   
+    	   System.out.println(4 == 4);
+    	  
+    	   System.out.println(4 != 4);
+    	   
+    	   System.out.println(b > a);
+    	   System.out.println((0 < 3) && (1 >= 0 || 2 <= 5) && 22 >= 90);
+    	   System.out.println((10 == 9) || (1 >= 0 && 100 > 5) && 2 > 0);
+    	   System.out.println((30 >= 29) && (20 > 19) || (1 >= 0 || 2 <= 5) && 22 >= 90);
+    	   System.out.println(10 > 45 && 20 < 9 || check && true);
+    	   System.out.println(true || 0 < 9 || check && 0 > 0);
+    	   System.out.println(1 > 5 || 2 < 9 | false && 0 == 0);
+    	   System.out.println(check && (20 < 9) || 45 != 0 && true);
+
+
+    	   
+    	
+      }
+      
+}
